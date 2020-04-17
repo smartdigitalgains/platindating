e0dcb2464405dda0abc14e20244db97db787cd0a
Tizian Thost <tizian@tizians-mbp.speedport.ip>
2020-04-17 20:01:49 +0200

    push

--- core/resources/viewElements/Navigation.php
+++ core/resources/viewElements/Navigation.php
@@ -17,7 +17,7 @@ class Navigation
   public function render(){
 
     echo '
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -27,7 +27,7 @@ class Navigation
     ';
     
     foreach ($_SESSION['routes'] as  $route) {
-      echo '<li class="nav-item active"><a href="/'.$route->slug.'">'.$route->label.'</a></li>';
+      echo '<li class="nav-item active"><a class="nav-link" href="/'.$route->slug.'">'.$route->label.'</a></li>';
     }
     
     echo'
