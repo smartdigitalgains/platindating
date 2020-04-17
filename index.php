ef73a05ee351e53021508eada2665772b016dcf8
Tizian Thost <tizian@tizians-mbp.speedport.ip>
2020-04-17 11:53:12 +0200

    push

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//c0r3 push
+
+session_start();
+
+ini_set('display_errors',1);
+ini_set('max_execution_time', 0);
+error_reporting(E_ALL|E_STRICT);
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/c0r3/core.php';
+
+Core::load();
+
+( new Core\Router() )->route();
+
+?>
