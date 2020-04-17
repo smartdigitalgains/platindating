c0b305b7f7fe0d68e7a58298daffecbe3632b9af
Tizian Thost <tizian@tizians-mbp.speedport.ip>
2020-04-17 19:01:04 +0200

    push

--- core/resources/formElements/textinput.php
+++ core/resources/formElements/textinput.php
@@ -24,7 +24,7 @@ class TextInput
     echo '
     <div class="form-group">
       <label for="data['.$this->name.']">'.$this->label.'</label>
-      <input type="text" value="'.$value.'" placeholder="'.$this->label.'" name="data['.$this->name.']" class="'.$this->classes.'" />
+      <input class="form-control" type="text" value="'.$value.'" placeholder="'.$this->label.'" name="data['.$this->name.']" class="'.$this->classes.'" />
       </div>
     ';
 
