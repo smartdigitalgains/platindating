8a7e2a49a7765305c581f716d1c1ace38d8482c3
Tizian Thost <tizian@tizians-mbp.speedport.ip>
2020-04-17 20:26:02 +0200

    push

--- core/resources/viewElements/Navigation.php
+++ core/resources/viewElements/Navigation.php
@@ -33,6 +33,10 @@ class Navigation
     echo'
     </ul>
     </div>
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+      </form>
     </nav>
     ';
   }
