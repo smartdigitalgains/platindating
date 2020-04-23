3bb57378b62a9fbedf5f579176f15700799d83b9
Tizian Thost <tizian@Tizians-MacBook-Pro.local>
2020-04-23 14:43:54 +0200

    push

--- core/forms/modules/builder/Builder.php
+++ core/forms/modules/builder/Builder.php
@@ -17,7 +17,7 @@ class Builder
   {
 
     echo '
-    <div class="container sticky-top">
+    <div class="sticky-top">
     <form action="'.$_SERVER['REQUEST_URI'].'" method="post">';
 
     //Set Cross Site Security Token
